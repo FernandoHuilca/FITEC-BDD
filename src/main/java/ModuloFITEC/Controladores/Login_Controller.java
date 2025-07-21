22ace96c13bfc64ac7ad8759f2284e33a9f87fc7
FernandoH <fernando.huilca@epn.edu.ec>
2025-07-21 00:11:10 -0500

    modelo clientes'GUI

--- src/main/java/ModuloFITEC/Controladores/Login_Controller.java
+++ src/main/java/ModuloFITEC/Controladores/Login_Controller.java
@@ -1,7 +1,18 @@
 package ModuloFITEC.Controladores;
 
 
-public class Login_Controller {
+import MetodosGlobales.MetodosFrecuentes;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+public class Login_Controller {
+    @FXML
+    private Button buttonIngresar;
+
+    @FXML
+    private void ingresar(){
+        MetodosFrecuentes.cambiarVentana((Stage) buttonIngresar.getScene().getWindow(), "/ModuloFITEC/views/Cliente.fxml", "Cliente");
+    }
 
 }
