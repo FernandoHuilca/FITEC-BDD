e1ba301085161d5e87e9766b1be965d3a98d4d0f
mateonicolasg <mateo.simbana@epn.edu.ec>
2025-07-22 17:52:16 -0500

    Añadiendo botones en las vistas de suplemento y compra

--- src/main/java/ModuloFITEC/Controllers/ControladorCompraEliminacion.java
+++ src/main/java/ModuloFITEC/Controllers/ControladorCompraEliminacion.java
@@ -1,5 +1,89 @@
 package ModuloFITEC.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControladorCompraEliminacion {
-    
+
+    @FXML
+    private Button buttonClientes;
+
+    @FXML
+    private Button buttonCompras;
+
+    @FXML
+    private Button buttonConsultarCompra;
+
+    @FXML
+    private Button buttonEliminarCompra;
+
+    @FXML
+    private Button buttonInicio;
+
+    @FXML
+    private Button buttonInstructores;
+
+    @FXML
+    private Button buttonNominaInstructores;
+
+    @FXML
+    private Button buttonRegistrarCompra;
+
+    @FXML
+    private Button buttonSuplementos;
+
+    @FXML
+    private Button buttonSuscripciones;
+
+    @FXML
+    void cambiarVentanaClientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaCompras(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaConsultarCompra(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaEliminarCompra(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaInicio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaInstructores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaNominaInstructores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaRegistrarCompra(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaSuplementos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaSuscripciones(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/ModuloFITEC/Controllers/ControladorSuplementoEliminacion.java
+++ src/main/java/ModuloFITEC/Controllers/ControladorSuplementoEliminacion.java
@@ -1,5 +1,97 @@
 package ModuloFITEC.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControladorSuplementoEliminacion {
-    
+
+    @FXML
+    private Button buttonActualizarSuplemento;
+
+    @FXML
+    private Button buttonClientes;
+
+    @FXML
+    private Button buttonCompras;
+
+    @FXML
+    private Button buttonConsultarSuplemento;
+
+    @FXML
+    private Button buttonEliminarSuplemento;
+
+    @FXML
+    private Button buttonInicio;
+
+    @FXML
+    private Button buttonInstructores;
+
+    @FXML
+    private Button buttonNominaInstructores;
+
+    @FXML
+    private Button buttonRegistrarSuplemento;
+
+    @FXML
+    private Button buttonSuplementos;
+
+    @FXML
+    private Button buttonSuscripciones;
+
+    @FXML
+    void cambiarVentanaActualizarSuplemento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaClientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaCompras(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaConsultarSuplemento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaEliminarSuplemento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaInicio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaInstructores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaNominaInstructores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaRegistrarSuplemento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaSuplementos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarVentanaSuscripciones(ActionEvent event) {
+
+    }
+
 }
